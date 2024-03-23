ab9b052e556e1e4050cbc7769827ca6c59088e0f
rae8108 <146367231+rae8108@users.noreply.github.com>
2024-03-23 22:17:47 +0000

    imported code into app.js

--- sneakers/src/App.js
+++ sneakers/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Product from "./Product";
+import Basket from "./Basket";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/basket" element={<Basket />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
